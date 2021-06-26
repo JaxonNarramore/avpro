7dba45db9c43a6823f4e698d50a89a9807a4c506
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-06-25 21:35:46 -0500

    seo update

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -13,16 +13,12 @@ export default function Services() {
                     <h4 className='service-txt'>Custom AV Systems</h4>
                 </div>
                 <div className='grid-images'>
-                    <img title="Home Automation" className='grid-image' src="Images/homeauto2.jpg" alt="Home Automation" title="Home Automation" />
-                    <h4 className='service-txt'>Home Automation</h4>
-                </div>
-                <div className='grid-images'>
-                    <img title="Whole Home Audio" className='grid-image' src="Images/wholehome.jpg" alt="Whole Home Audio Installation" title="Whole Home Audio Installation" />
-                    <h4 className='service-txt'>Whole Home Audio</h4>
-                </div>
-                <div className='grid-images'>
                     <img title="TV Mounting" className='grid-image' src="Images/mounting.jpg" alt="TV Mounting" title="TV Mounting" />
                     <h4 className='service-txt'>TV Mounting</h4>
+                </div>
+                <div className='grid-images'>
+                    <img title="Home Automation" className='grid-image' src="Images/homeauto2.jpg" alt="Home Automation" title="Home Automation" />
+                    <h4 className='service-txt'>Home Automation</h4>
                 </div>
                 <div className='grid-images'>
                     <img title="" className='grid-image' src="Images/hometheature.jpg" alt="Home Theatre Installation" title="Home Theatre Installation" />
@@ -31,6 +27,10 @@ export default function Services() {
                 <div className='grid-images'>
                     <img title="Security Camera Installation" className='grid-image' src="Images/securitycamera.jpg" alt="Security Camera Installation" title="Security Camera Installation" />
                     <h4 className='service-txt'>Security Camera Installation</h4>
+                </div>
+                <div className='grid-images'>
+                    <img title="Whole Home Audio" className='grid-image' src="Images/wholehome.jpg" alt="Whole Home Audio Installation" title="Whole Home Audio Installation" />
+                    <h4 className='service-txt'>Whole Home Audio</h4>
                 </div>
             </div>
         </div>
