23a3046b52a3c8bb7035189d093f6ca53771ddcd
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-07-01 09:54:34 -0500

    seo refresh

--- src/Components/WCU.js
+++ src/Components/WCU.js
@@ -10,14 +10,14 @@ export default function WCU() {
                 <p className='wcu-p'>We will get back to you the same day you call or fill out the form!</p>
             </div>
             <div className='wcu-div'>
-                <img className='wcu-img' src="Images/brain.png" alt="Audio Visual Installation Expierence Emblem" title="Audio Visual Installation Expierence" />
-                <p className='wcu-h'>Experience</p>
-                <p className='wcu-p'>All of our techniciians have multiple years of expierence in the industry</p>
-            </div>
-            <div className='wcu-div'>
                 <img className='wcu-img' src="Images/warrenty.png" alt="Audio Visual Installation Lifetime Warrenty Emblem" title="Audio Visual Installation Lifetime Warrenty" />
                 <p className='wcu-h'>Lifetime Warrenty</p>
                 <p className='wcu-p'>We offer a lifetime installation warrenty on all of our installations</p>
+            </div>
+            <div className='wcu-div'>
+                <img className='wcu-img' src="Images/brain.png" alt="Audio Visual Installation Expierence Emblem" title="Audio Visual Installation Expierence" />
+                <p className='wcu-h'>Experience</p>
+                <p className='wcu-p'>All of our techniciians have multiple years of expierence in the industry</p>
             </div>
         </div>
     )
