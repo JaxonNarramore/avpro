03bfc11a5aa15e15e96993a3322379848b87184e
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-07-07 16:49:50 -0500

    seo

--- src/Components/WCU.js
+++ src/Components/WCU.js
@@ -5,6 +5,11 @@ export default function WCU() {
     return (
         <div className='wcu-container'>
             <div className='wcu-div'>
+                <img className='wcu-img' src="Images/brain.png" alt="Audio Visual Installation Expierence Emblem" title="Audio Visual Installation Expierence" />
+                <p className='wcu-h'>Experience</p>
+                <p className='wcu-p'>All of our techniciians have multiple years of expierence in the industry</p>
+            </div>
+            <div className='wcu-div'>
                 <img className='wcu-img' src="Images/warrenty.png" alt="Audio Visual Installation Lifetime Warrenty Emblem" title="Audio Visual Installation Lifetime Warrenty" />
                 <p className='wcu-h'>Lifetime Warrenty</p>
                 <p className='wcu-p'>We offer a lifetime installation warrenty on all of our installations</p>
@@ -13,11 +18,6 @@ export default function WCU() {
                 <img id='warrenty' className='wcu-img' src="Images/clock.png" alt="Audio Visual Installation Quick Responses Emblem" title="Audio Visual Installation Quick Responses" />
                 <p className='wcu-h'>Quick Responses</p>
                 <p className='wcu-p'>We will get back to you the same day you call or fill out the form!</p>
-            </div>
-            <div className='wcu-div'>
-                <img className='wcu-img' src="Images/brain.png" alt="Audio Visual Installation Expierence Emblem" title="Audio Visual Installation Expierence" />
-                <p className='wcu-h'>Experience</p>
-                <p className='wcu-p'>All of our techniciians have multiple years of expierence in the industry</p>
             </div>
         </div>
     )
